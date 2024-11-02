ee779103cdff841fec4689d41a816ee903820320
Supratim Nag <snsupratim@gmail.com>
2024-11-02 16:25:37 +0530

    maxsubarray

---
+++ array/maxSubArray.java
@@ -0,0 +1,15 @@
+public class maxSubArray {
+    public static int maxSubArray(int[] nums) {
+
+        // copy from below
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+}
